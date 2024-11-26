b57974d44d636e902b3bd856c581503433f36893
Eduard Babetskyi <edward.babetskyi@gmail.com>
2024-11-26 11:32:13 +0200

    first commit

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -24,10 +24,10 @@ const Footer = () => {
                     <img src={footer_img_2} alt="Amature"/>
                     <img src={footer_img_3} alt="Empfehlung"/>
                 </div>
-                {footer_link && footer_link[0] ? <a href={footer_link[0]['link']}>{footer_link[0][currentLanguage]}</a> : ''}
-                {footer_link &&footer_link[1] ? <a href={footer_link[1]['link']}>{footer_link[1][currentLanguage]}</a> : ''}
-                {footer_link &&footer_link[2] ? <a href={footer_link[2]['link']}>{footer_link[2][currentLanguage]}</a> : ''}
-                {footer_link &&footer_link[3] ? <a href={footer_link[3]['link']}>{footer_link[3][currentLanguage]}</a> : ''}
+                {footer_link && footer_link[0] ? <a target='_blank' href={footer_link[0]['link']}>{footer_link[0][currentLanguage]}</a> : ''}
+                {footer_link &&footer_link[1] ? <a target='_blank' href={footer_link[1]['link']}>{footer_link[1][currentLanguage]}</a> : ''}
+                {footer_link &&footer_link[2] ? <a target='_blank' href={footer_link[2]['link']}>{footer_link[2][currentLanguage]}</a> : ''}
+                {footer_link &&footer_link[3] ? <a target='_blank' href={footer_link[3]['link']}>{footer_link[3][currentLanguage]}</a> : ''}
             </div>
         </footer>
     );
