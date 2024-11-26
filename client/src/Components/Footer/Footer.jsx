cd1778ad53ca612e6259752638ba0896acc4a4ee
Eduard Babetskyi <edward.babetskyi@gmail.com>
2024-11-26 18:00:17 +0200

    first commit

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './footer.scss'
 
 import footer_img_1 from '../../Assets/footer_img_1.png'
 import footer_img_2 from '../../Assets/footer_img_2.png'
 import footer_img_3 from '../../Assets/footer_img_3.png'
-import {useModal} from "../Modal/ModalContext.jsx";
 
 import footer_link from '../../footer_link.json'
 import {useTranslation} from "react-i18next";
 const Footer = () => {
-    const {openModal} = useModal();
-
     const {i18n} = useTranslation();
     const currentLanguage = i18n.language;
-    const handleOpen = () => {
-        openModal();
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Перевірка на мобільний пристрій
+        const checkDevice = () => {
+            setIsMobile(window.innerWidth <= 768); // Встановлюємо поріг для мобільних пристроїв
+        };
+
+        // Викликаємо перевірку при першому рендерингу та при зміні розміру вікна
+        checkDevice();
+        window.addEventListener('resize', checkDevice);
+
+        return () => {
+            window.removeEventListener('resize', checkDevice);
+        };
+    }, []);
+
+    // const openPopup = (url) => {
+    //     window.open(url, 'popup', 'width=450,height=500,scrollbars=yes,resizable=yes');
+    // };
+
+    const openLink = (url) => {
+        if (isMobile) {
+            window.open(url, '_blank');
+        } else {
+            window.open(url, 'popup', 'width=450,height=500,scrollbars=yes,resizable=yes');
+        }
     };
+
     return (
         <footer>
             <div className="container">
@@ -24,10 +48,10 @@ const Footer = () => {
                     <img src={footer_img_2} alt="Amature"/>
                     <img src={footer_img_3} alt="Empfehlung"/>
                 </div>
-                {footer_link && footer_link[0] ? <a target='_blank' href={footer_link[0]['link']}>{footer_link[0][currentLanguage]}</a> : ''}
-                {footer_link &&footer_link[1] ? <a target='_blank' href={footer_link[1]['link']}>{footer_link[1][currentLanguage]}</a> : ''}
-                {footer_link &&footer_link[2] ? <a target='_blank' href={footer_link[2]['link']}>{footer_link[2][currentLanguage]}</a> : ''}
-                {footer_link &&footer_link[3] ? <a target='_blank' href={footer_link[3]['link']}>{footer_link[3][currentLanguage]}</a> : ''}
+                {footer_link && footer_link[0] ? <button onClick={() => openLink(footer_link[0]['link'])}>{footer_link[0][currentLanguage]}</button> : ''}
+                {footer_link &&footer_link[1] ? <button onClick={() => openLink(footer_link[1]['link'])}>{footer_link[1][currentLanguage]}</button> : ''}
+                {footer_link &&footer_link[2] ? <button onClick={() => openLink(footer_link[2]['link'])}>{footer_link[2][currentLanguage]}</button> : ''}
+                {footer_link &&footer_link[3] ? <button onClick={() => openLink(footer_link[3]['link'])}>{footer_link[3][currentLanguage]}</button> : ''}
             </div>
         </footer>
     );
