f5fa59a17e62b42f9b251f63eaeb86e62defc850
Eduard Babetskyi <edward.babetskyi@gmail.com>
2024-11-26 02:15:27 +0200

    first commit

--- client/src/Components/Slider/TopSlider.jsx
+++ client/src/Components/Slider/TopSlider.jsx
@@ -136,7 +136,6 @@ export default function TopSlider() {
                                             src={record?.photo}
                                             alt=""
                                             loading="lazy"
-                                            sizes={"(max-width: 170px)"}
                                         />
                                     </div>
                                     {record?.badge && (
